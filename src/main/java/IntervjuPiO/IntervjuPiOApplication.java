7cd626540c92483081e290692d704fa9b5c0c55a
NikKnez <nknezevic07@gmail.com>
2024-05-09 14:29:02 +0200

    Initial commit

---
+++ src/main/java/IntervjuPiO/IntervjuPiOApplication.java
@@ -0,0 +1,13 @@
+package IntervjuPiO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntervjuPiOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntervjuPiOApplication.class, args);
+	}
+
+}
