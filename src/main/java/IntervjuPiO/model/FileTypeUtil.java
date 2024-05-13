42f9ecf286f3ce7804b059545fe01d73a0b81d05
NikKnez <nknezevic07@gmail.com>
2024-05-13 19:34:13 +0200

    Added FileTypeUtil.java class for future new types of files and modified FileController class

---
+++ src/main/java/IntervjuPiO/model/FileTypeUtil.java
@@ -0,0 +1,20 @@
+//package IntervjuPiO.model;
+//
+//import lombok.Data;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Data
+//public final class FileTypeUtil {
+//
+//    public static String getMimeType(String fileExtension) {
+//        Map<String, String> mimeTypes = new HashMap<>();
+//        mimeTypes.put("pdf", "application/pdf");
+//        mimeTypes.put("docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+//        // Add more MIME types as needed
+//
+//        return mimeTypes.getOrDefault(fileExtension, "application/octet-stream"); // Default to binary data if unknown
+//    }
+//}
+//
