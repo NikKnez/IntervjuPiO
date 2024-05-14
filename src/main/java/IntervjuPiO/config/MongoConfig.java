4f4380a39cededeeadac9cbaf40251748f593a25
NikKnez <nknezevic07@gmail.com>
2024-05-14 14:15:34 +0200

    Modified application.properties, IntervjuPiOApplication class and added MongoConfig class to connect do DB

---
+++ src/main/java/IntervjuPiO/config/MongoConfig.java
@@ -0,0 +1,30 @@
+package IntervjuPiO.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "IntervjuPiO.repository")
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "intervju_db";
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        return MongoClients.create("mongodb+srv://nknezevic07:N!kol%4007@cluster0.lknjmwl.mongodb.net/");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), getDatabaseName());
+    }
+}
+
