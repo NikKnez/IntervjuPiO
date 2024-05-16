d4bc1e2d21f42e9d55883a5540d79b98df02ad49
NikKnez <nknezevic07@gmail.com>
2024-05-16 13:19:17 +0200

    Modified FileController and WebConfig class

--- src/main/java/IntervjuPiO/config/WebConfig.java
+++ src/main/java/IntervjuPiO/config/WebConfig.java
@@ -28,39 +28,15 @@ public class WebConfig implements WebMvcConfigurer {
 //                .allowedHeaders("Content-Disposition", "Content-Type", "Authorization", "X-Requested-With", "Access-Control-Allow-Origin", "*")
 //                .allowCredentials(true);
 //    }
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource(@Value("${app.cors.allowed-origins}") List<String> allowedOrigins) {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowCredentials(true);
-//        configuration.setAllowedOrigins(allowedOrigins);
-//        configuration.addAllowedMethod("*");
-//        configuration.addAllowedHeader("*");
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-private static final int CORS_FILTER_ORDER = -102;
 
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://intervjupio.netlify.app");
-        config.setAllowedHeaders(Arrays.asList(
-                HttpHeaders.AUTHORIZATION,
-                HttpHeaders.CONTENT_TYPE,
-                HttpHeaders.ACCEPT));
-        config.setAllowedMethods(Arrays.asList(
-                HttpMethod.GET.name(),
-                HttpMethod.POST.name(),
-                HttpMethod.PUT.name(),
-                HttpMethod.DELETE.name()));
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-        bean.setOrder(CORS_FILTER_ORDER);
-        return bean;
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
+                .allowCredentials(true);;
     }
+
 
 }
 
--- src/main/java/IntervjuPiO/controller/FileController.java
+++ src/main/java/IntervjuPiO/controller/FileController.java
@@ -10,16 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import IntervjuPiO.model.FileDocument;
 import IntervjuPiO.repository.FileRepository;
 
-@CrossOrigin(origins = "https://intervjupio.netlify.app")
+//@CrossOrigin(origins = "https://intervjupio.netlify.app")
 @RestController
+@RequestMapping("/*")
 public class FileController {
 
     @Autowired
