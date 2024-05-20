1f4413624e2d72e6e5e6dd6ac657d816e2b6811b
NikKnez <nknezevic07@gmail.com>
2024-05-20 12:49:58 +0200

    Added "PORT" to IntervjuPiOApplication class

--- src/main/java/IntervjuPiO/IntervjuPiOApplication.java
+++ src/main/java/IntervjuPiO/IntervjuPiOApplication.java
@@ -3,6 +3,9 @@ package IntervjuPiO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
@@ -11,6 +14,11 @@ public class IntervjuPiOApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntervjuPiOApplication.class, args);
+	}
+
+	@Bean
+	public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainerCustomizer() {
+		return factory -> factory.setPort(Integer.parseInt(System.getenv("PORT")));
 	}
 
 }
