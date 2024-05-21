c455f3a13a8eaef024fe42749e00051d9400134e
NikKnez <nknezevic07@gmail.com>
2024-05-21 13:42:37 +0200

    Modified SimpleCORSFilter class a

--- src/main/java/IntervjuPiO/config/SimpleCORSFilter.java
+++ src/main/java/IntervjuPiO/config/SimpleCORSFilter.java
@@ -26,7 +26,7 @@ public class SimpleCORSFilter implements Filter {
 
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "https://intervjupio.netlify.app");
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
