33529b1a51bc31a680cea4432e16d2207e6a22de
NikKnez <nknezevic07@gmail.com>
2024-05-21 13:34:49 +0200

    Modified FileController, deleted SecurityConfig and disabled WebConfig class, added SimpleCORSFilter class and updated pom.xml

--- src/main/java/IntervjuPiO/config/WebConfig.java
+++ src/main/java/IntervjuPiO/config/WebConfig.java
@@ -1,19 +1,11 @@
-package IntervjuPiO.config;
-
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-
+//package IntervjuPiO.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import java.util.Arrays;
+//
 //@Configuration
 //public class WebConfig implements WebMvcConfigurer {
 //
@@ -28,34 +20,5 @@ import java.util.Arrays;
 //    }
 //
 //}
-
-@Configuration
-public class WebConfig {
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = getCorsConfiguration();
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
-
-    private static CorsConfiguration getCorsConfiguration() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://intervjupio.netlify.app");
-        config.setAllowedHeaders(Arrays.asList(
-                HttpHeaders.AUTHORIZATION,
-                HttpHeaders.CONTENT_TYPE,
-                HttpHeaders.ACCEPT));
-        config.setAllowedMethods(Arrays.asList(
-                HttpMethod.GET.name(),
-                HttpMethod.POST.name(),
-                HttpMethod.PUT.name(),
-                HttpMethod.DELETE.name(),
-                HttpMethod.OPTIONS.name()));
-        return config;
-    }
-}
-
-
+//
+//
