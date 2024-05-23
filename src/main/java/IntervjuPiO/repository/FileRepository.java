45ee78d676a90369f4768c1c76c39ad745752946
NikKnez <nknezevic07@gmail.com>
2024-05-23 12:54:33 +0200

    Modified FileController and FileRepository class

--- src/main/java/IntervjuPiO/repository/FileRepository.java
+++ src/main/java/IntervjuPiO/repository/FileRepository.java
@@ -2,7 +2,8 @@ package IntervjuPiO.repository;
 
 import IntervjuPiO.model.FileDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface FileRepository extends MongoRepository<FileDocument, String> {
-    // Define additional query methods if needed
+public interface FileRepository extends MongoRepository<FileDocument, String>, PagingAndSortingRepository<FileDocument, String> {
+    // Define query methods if needed
 }
