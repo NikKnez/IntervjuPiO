de7a2c62ad98bbff583c998037bb30729f82b589
NikKnez <nknezevic07@gmail.com>
2024-05-23 10:32:22 +0200

    Modified FileDocument class

--- src/main/java/IntervjuPiO/model/FileDocument.java
+++ src/main/java/IntervjuPiO/model/FileDocument.java
@@ -1,9 +1,11 @@
 package IntervjuPiO.model;
 
-import lombok.Data;
+import lombok.*;
 import org.bson.types.Binary;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Document(collection = "files")
 public class FileDocument {
@@ -12,10 +14,4 @@ public class FileDocument {
     private String filetype;
     private Binary content;
 
-    public FileDocument(String id, String filename, String filetype, Binary content) {
-        this.id = id;
-        this.filename = filename;
-        this.filetype = filetype;
-        this.content = content;
-    }
 }
